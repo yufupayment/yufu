3b24bb0d6e561e5f0786d4f9484da3647128a13b
yufujituan <785388876@qq.com>
2017-04-17 17:30:18 +0800

    commit

--- static/js/header.js
+++ static/js/header.js
@@ -29,11 +29,11 @@ function headerFn(){
                                         '<dt><a href="#">网络支付</a></dt>'+
                                         '<dd>'+
                                             '<ul class="font14 zoom menu-list1">'+
-                                                '<li><a href="../../yufu/pages/pr_netpay.html">企业网银支付</a></li>'+
-                                                '<li><a href="../../yufu/pages/pr_netpay.html">个人网银支付</a></li>'+
-                                                '<li><a href="#">批量付款</a></li>'+
-                                                '<li><a href="#">快捷支付</a></li>'+
-                                                '<li><a href="#">扫码支付</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_netpay.html?page=2">企业网银支付</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_netpay.html?page=3">个人网银支付</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_netpay.html?page=4">批量付款</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_netpay.html?page=5">快捷支付</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_netpay.html?page=6">扫码支付</a></li>'+
                                             '</ul>'+
                                         '</dd>'+
                                     '</dl>'+
@@ -43,10 +43,10 @@ function headerFn(){
                                         '<dt><a href="#">移动支付</a></dt>'+
                                         '<dd>'+
                                             '<ul class="font14 zoom menu-list1">'+
-                                                '<li><a href="../../yufu/pages/pr_mobilepay.html">移动支付插件</a></li>'+
-                                                '<li><a href="#">裕福支付钱包</a></li>'+
-                                                '<li><a href="#">Wap网银支付</a></li>'+
-                                                '<li class="width100"><a href="#">商户移动管理平台</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_mobilepay.html?page=2">移动支付插件</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_mobilepay.html?page=5">裕福支付钱包</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_mobilepay.html?page=3">Wap网银支付</a></li>'+
+                                                '<li class="width100"><a href="../../yufu/pages/pr_mobilepay.html?page=4">商户移动管理平台</a></li>'+
                                             '</ul>'+
                                         '</dd>'+
                                     '</dl>'+
@@ -56,19 +56,19 @@ function headerFn(){
                                         '<dt><a href="#">金融服务</a></dt>'+
                                         '<dd>'+
                                             '<ul class="font14 zoom">'+
-                                                '<li><a href="../../yufu/pages/pr_finance.html">在线供应链融资服务</a></li>'+
-                                                '<li><a href="#">贷后监控预警服务</a></li>'+
-                                                '<li><a href="#">数据融资授信服务</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_finance.html?page=2">在线供应链融资服务</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_finance.html?page=3">贷后监控预警服务</a></li>'+
+                                                '<li><a href="../../yufu/pages/pr_finance.html?page=4">数据融资授信服务</a></li>'+
                                             '</ul>'+
                                         '</dd>'+
                                     '</dl>'+
                                 '</div>'+
                             '</div>'+
                         '</li>'+
-                        '<li class="pos_r" data-page="case"><a href="#">行业解决方案</a></li>'+
+                        '<li class="pos_r" data-page="case"><a href="../../yufu/pages/pro_internetFinance.html">行业解决方案</a></li>'+
                         '<li class="pos_r" data-page="safe"><a href="../../yufu/pages/sa_safe.html">安全中心</a></li>'+
                         '<li class="pos_r" data-page="service"><a href="../../yufu/pages/cus_company.html">客户服务</a></li>'+
-                        '<li class="pos_r" data-page="aboutus"><a href="#">关于我们</a>'+
+                        '<li class="pos_r" data-page="aboutus"><a href="../../yufu/pages/ab_aboutus.html">关于我们</a>'+
                             '<div class="navmenu navbox2 pos_a">'+
                                 '<i></i>'+
                                 '<div class="item">'+
